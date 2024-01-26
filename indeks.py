4c021ed4b10964640b4ad9b3b681e5733ea24101
opilane <opilane@HP-CZC2349HT4>
2024-01-26 11:42:45 +0200

    11

--- indeks.py
+++ indeks.py
@@ -1,12 +1,15 @@
 ﻿print("Tere! Olen sinu uus sõber - Python!")
 nimi=input("Sisesta oma nimi: ")
 print(nimi, " oi kui ilus nimi")
-print (nimi, "! Kas leian Sinu keha indeksi? 0-ei, 1-jah =>")
-if vastus ==1:
-    pikkus = int(input("Введите cвой рост в (см)")) #данные для массы тела
-    mass = float(input("Введите свой вес в (kg)"))
-    indeks = mass /(0.01 * pikkus) ** 2
-    print(nimi, "! Sinu keha indeks on:" )
+try:
+    vastus = input(nimi, "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => ")
+    
+    if vastus == "1":
+        pikkus = int(input("Введите cвой рост в (см)")) #данные для массы тела
+        mass = float(input("Введите свой вес в (kg)")) #данные для массы тела
+        indeks = mass /(0.01 * pikkus) ** 2
+         print(nimi, "! Sinu keha indeks on:" )
+
 
 
 if indeks<16:
@@ -21,6 +24,13 @@ elif 30<= indeks<35:
     print("Rasvumine")
 elif 35<= indeks<40:
     print("Tugev rasvumine")
-elif >40:
+elif indeks>40:
     print("Tervisele ohtlik rasvumine")
+else:
+    print("Kahju! See on väga kasulik info!")
+    print("")
+print("Kohtumiseni, " + nimi + "! Igavesti Sinu, Python!")
 
+except ValueError:
+     print("Vale sisend! Palun sisesta sobivad väärtused.")
+    #print("ValueError")
